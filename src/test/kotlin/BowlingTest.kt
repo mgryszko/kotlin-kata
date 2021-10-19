520d11a2f7759d9aee813484adfab143741b472e
mgryszko <marcin.gryszko@lastminute.com>
2021-10-19 18:33:13 +0200

    Two rolls not grouped into the frame

--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -4,11 +4,16 @@ import kotlin.test.Test
 
 class BowlingTest {
     @Test
-    fun `one roll without the frame`() {
-        expect(score(0)).toBe(0)
-        expect(score(5)).toBe(5)
-        expect(score(10)).toBe(10)
+    fun `two rolls`() {
+        expect(score(0, 0)).toBe(0)
+        expect(score(0, 1)).toBe(1)
+        expect(score(2, 3)).toBe(5)
+        expect(score(4, 5)).toBe(9)
+        expect(score(0, 9)).toBe(9)
+        expect(score(9, 1)).toBe(10)
+        expect(score(0, 10)).toBe(10)
+        expect(score(10, 0)).toBe(10)
     }
 }
 
-fun score(pins: Int): Int = pins
+fun score(roll1: Int, roll2: Int): Int = roll1 + roll2
