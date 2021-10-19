36b0d1e9e5244a5180cb14197fc86f3d05dc25ec
mgryszko <marcin.gryszko@lastminute.com>
2021-10-19 18:51:59 +0200

    Multiple frames. Strikes and spares are represented as open frames

--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -4,18 +4,20 @@ import kotlin.test.Test
 
 class BowlingTest {
     @Test
-    fun `one frame`() {
-        expect(score(OpenFrame(0, 0))).toBe(0)
-        expect(score(OpenFrame(0, 1))).toBe(1)
-        expect(score(OpenFrame(2, 3))).toBe(5)
-        expect(score(OpenFrame(4, 5))).toBe(9)
-        expect(score(OpenFrame(0, 9))).toBe(9)
-        expect(score(OpenFrame(9, 1))).toBe(10)
-        expect(score(OpenFrame(0, 10))).toBe(10)
-        expect(score(OpenFrame(10, 0))).toBe(10)
+    fun `two frames`() {
+        expect(score(listOf(OpenFrame(0, 0), OpenFrame(0, 1)))).toBe(1)
+        expect(score(listOf(OpenFrame(0, 1), OpenFrame(2, 3)))).toBe(6)
+        expect(score(listOf(OpenFrame(9, 1), OpenFrame(4, 3)))).toBe(17)
+        expect(score(listOf(OpenFrame(10, 0), OpenFrame(4, 3)))).toBe(17)
+    }
+
+    @Test
+    fun `three frames`() {
+        expect(score(listOf(OpenFrame(0, 0), OpenFrame(0, 1), OpenFrame(2, 0)))).toBe(3)
+        expect(score(listOf(OpenFrame(0, 1), OpenFrame(9, 0), OpenFrame(2, 7)))).toBe(19)
     }
 }
 
 data class OpenFrame(val roll1: Int, val roll2: Int)
 
-fun score(frame: OpenFrame): Int = frame.roll1 + frame.roll2
+fun score(frames: List<OpenFrame>): Int = frames.map { it.roll1 + it.roll2 }.sum()
