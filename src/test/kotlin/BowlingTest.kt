b768e1e135ba4cb0fdd02ed81702539bd10788e0
mgryszko <marcin.gryszko@lastminute.com>
2021-10-19 18:40:39 +0200

    One frame. Strikes and spares are represented as open frames

--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -4,16 +4,18 @@ import kotlin.test.Test
 
 class BowlingTest {
     @Test
-    fun `two rolls`() {
-        expect(score(0, 0)).toBe(0)
-        expect(score(0, 1)).toBe(1)
-        expect(score(2, 3)).toBe(5)
-        expect(score(4, 5)).toBe(9)
-        expect(score(0, 9)).toBe(9)
-        expect(score(9, 1)).toBe(10)
-        expect(score(0, 10)).toBe(10)
-        expect(score(10, 0)).toBe(10)
+    fun `one frame`() {
+        expect(score(OpenFrame(0, 0))).toBe(0)
+        expect(score(OpenFrame(0, 1))).toBe(1)
+        expect(score(OpenFrame(2, 3))).toBe(5)
+        expect(score(OpenFrame(4, 5))).toBe(9)
+        expect(score(OpenFrame(0, 9))).toBe(9)
+        expect(score(OpenFrame(9, 1))).toBe(10)
+        expect(score(OpenFrame(0, 10))).toBe(10)
+        expect(score(OpenFrame(10, 0))).toBe(10)
     }
 }
 
-fun score(roll1: Int, roll2: Int): Int = roll1 + roll2
+data class OpenFrame(val roll1: Int, val roll2: Int)
+
+fun score(frame: OpenFrame): Int = frame.roll1 + frame.roll2
