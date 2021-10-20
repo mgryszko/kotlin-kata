b45bf9a297ff9eaafc3b34f6af2ec2c66525841e
mgryszko <marcin.gryszko@lastminute.com>
2021-10-20 18:14:20 +0200

    Spares

--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -16,8 +16,24 @@ class BowlingTest {
         expect(score(listOf(OpenFrame(0, 0), OpenFrame(0, 1), OpenFrame(2, 0)))).toBe(3)
         expect(score(listOf(OpenFrame(0, 1), OpenFrame(9, 0), OpenFrame(2, 7)))).toBe(19)
     }
+
+    @Test
+    fun `spare`() {
+        expect(score(listOf(Spare(9, 1), OpenFrame(4, 3)))).toBe(21)
+    }
 }
 
-data class OpenFrame(val roll1: Int, val roll2: Int)
+sealed interface Frame {
+    val roll1: Int
+}
 
-fun score(frames: List<OpenFrame>): Int = frames.map { it.roll1 + it.roll2 }.sum()
+data class OpenFrame(override val roll1: Int, val roll2: Int) : Frame
+
+data class Spare(override val roll1: Int, val roll2: Int) : Frame
+
+fun score(frames: List<Frame>): Int = frames.windowed(2, partialWindows = true).map { frames ->
+    when (val current = frames[0]) {
+        is OpenFrame -> current.roll1 + current.roll2
+        is Spare -> current.roll1 + current.roll2 + frames[1].roll1
+    }
+}.sum()
