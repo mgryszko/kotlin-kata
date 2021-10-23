f7d431dbdba77bcd657116cd9ee83cc582ff8492
mgryszko <marcin.gryszko@lastminute.com>
2021-10-23 15:49:16 +0200

    Final frame with strikes and spares

--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -32,6 +32,10 @@ class BowlingTest {
     @Test
     fun `final frame`() {
         expect(score(listOf(OpenFrame(1, 8), FinalFrame(2, 4)))).toBe(15)
+        expect(score(listOf(OpenFrame(1, 8), FinalFrame(7, 3, 10)))).toBe(29)
+        expect(score(listOf(OpenFrame(1, 8), FinalFrame(10, 8, 9)))).toBe(36)
+        expect(score(listOf(Strike, Strike, Strike, Strike, Strike, Strike, Strike, Strike, Strike, FinalFrame(10, 10, 10)))).toBe(300)
+        expect(score(listOf(Spare(1, 9), Spare(2, 8), Spare(3, 7), Spare(4, 6), Spare(5, 5), Spare(6, 4), Spare(7, 3), Spare(8, 2), Spare(9, 1), FinalFrame(0, 10, 10)))).toBe(154)
     }
 }
 
@@ -69,10 +73,14 @@ object Strike : Frame {
     override fun ordinaryScore(): Int = roll
 }
 
-data class FinalFrame(val roll1: Int, val roll2: Int) : Frame {
+data class FinalFrame(val roll1: Int, val roll2: Int, val roll3: Int = 0) : Frame {
+    init {
+        require(if (roll1 + roll2 <= 9) roll3 == 0 else true) { "Final frame with no strike nor strike cannot have bonus roll" }
+    }
+
     override fun pins(): Sequence<Int> = sequenceOf(roll1, roll2)
 
-    override fun ordinaryScore(): Int = roll1 + roll2
+    override fun ordinaryScore(): Int = roll1 + roll2 + roll3
 }
 
 fun score(frames: List<Frame>): Int = frames.mapIndexed { i, frame ->
