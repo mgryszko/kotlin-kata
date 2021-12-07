008088f611fe1fb8f5d67c7de487ee3e9a3f884a
Marcin Gryszko <marcin.gryszko@clarity.ai>
2021-12-07 19:19:20 +0100

    Day 7, part 1 O(n log n) solution

--- src/main/kotlin/day7.kt
+++ src/main/kotlin/day7.kt
@@ -2,23 +2,37 @@ package aoc.day7
 
 import aoc.readText
 import kotlin.math.absoluteValue
-import kotlin.math.floor
 
 fun main() {
   val positions = "day7.txt".readText().trim().split(',').map(String::toInt).sorted()
 
-  val part1 = (positions.minOrNull()!!..positions.maxOrNull()!!)
-    .map { it to positions.constantCostOfMovementTo(it) }
-  println(part1.minByOrNull(Pair<Int, Int>::second))
-  println(positions.middleValue())
-  println(positions.constantCostOfMovementTo(positions.middleValue()))
+  val part1 = ((positions.first() + 1)..positions.last()).runningFold(positions.constantCostOfMovementTo(positions.first())) { cost, pos ->
+    val p = positions.lowerBoundBinarySearch(pos)
+    val (positionsLt, positionsGeq) = p to positions.size - p
+    cost - positionsGeq + positionsLt
+  }
+  println(part1.minOrNull()!!)
+  assert(part1.minOrNull() == 336120)
 
   val part2 = (positions.minOrNull()!!..positions.maxOrNull()!!)
     .map { it to positions.linearCostOfMovementTo(it) }
   println(part2.minByOrNull(Pair<Int, Int>::second))
-  println(floor(positions.average()))
-  println(positions.linearCostOfMovementTo(floor(positions.average()).toInt()))
-  println(positions.linearCostOfMovementTo(floor(positions.average()).toInt() + 1))
+  assert(part2.minByOrNull(Pair<Int, Int>::second) == (462 to 96864235))
+}
+
+val samplePositions = listOf(0, 1, 1, 2, 2, 2, 4, 7, 14, 16)
+
+fun List<Int>.lowerBoundBinarySearch(element: Int): Int {
+  val i = binarySearch(element)
+  return if (i < 0) -i - 1
+  else {
+    var prevI = i
+    while(prevI > 0) {
+      if (this[prevI - 1] == element) prevI--
+      else return prevI
+    }
+    return 0
+  }
 }
 
 fun List<Int>.constantCostOfMovementTo(pos: Int): Int = sumOf { (pos - it).absoluteValue }
@@ -27,7 +41,3 @@ fun List<Int>.linearCostOfMovementTo(pos: Int): Int = sumOf {
   val distance = (pos - it).absoluteValue
   distance * (distance + 1) / 2
 }
-
-fun List<Int>.middleValue(): Int = get(size / 2)
-
-
